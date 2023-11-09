c50828481169e415bb083b35e9d7c86b1a575be7
Laszlo Borbely <laszlo.borbely@mgm-tp.com>
2023-11-09 22:06:55 +0100

    fix linter warnings

--- internal/errortypes/user.go
+++ internal/errortypes/user.go
@@ -1,9 +1,7 @@
 package errortypes
 
-import "fmt"
-
 type IncorrectUsernameOrPasswordError struct{}
 
 func (i IncorrectUsernameOrPasswordError) Error() string {
-	return fmt.Sprintf("incorrect username or password")
+	return "incorrect username or password"
 }
